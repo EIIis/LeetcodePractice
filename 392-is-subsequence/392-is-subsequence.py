3f09cdf7ba0cad01f4cb0ea15c56eed5455e0e0c
Ellis Alcantara <47289830+EIIis@users.noreply.github.com>
2022-08-23 12:52:41 -0700

    Time: 27 ms (58.78%), Space: 13.9 MB (21.60%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        if len(s) == 0:
+            return True
+        elif len(t) == 0:
+            return False
+        
+        pointer1 = 0
+
+        for i in range(len(t)):
+            if s[pointer1] == t[i]:
+                pointer1 += 1
+                if pointer1 == len(s):
+                    return True
+        return False
